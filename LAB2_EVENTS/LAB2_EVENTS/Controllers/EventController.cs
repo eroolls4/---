f6d875a018959cd95c76831a5e78d0b416c2d7d3
neuralNinj4 <2good4youbye@gmail.com>
2024-04-28 02:19:23 +0200

    lab2 done

---
+++ LAB2_EVENTS/LAB2_EVENTS/Controllers/EventController.cs
@@ -0,0 +1,114 @@
+﻿using LAB2_EVENTS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Sockets;
+
+namespace LAB2_EVENTS.Controllers
+{
+    public class EventController : Controller
+    {
+
+
+        private static List<Event> events = new List<Event>()
+        {
+            new Event("event1", "kumanovo"),
+            new Event("event2", "skopje"),
+            new Event("event3", "veles"),
+            new Event("event4", "dojshland"),
+            new Event("event5", "shfajcari")
+        };
+
+        public EventController()
+        {
+
+        }
+
+
+        public IActionResult GetAllEvents()
+        {
+
+            return View(events);
+        }
+
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("eventName , eventLocation")] Event e)
+        {
+            if (ModelState.IsValid)
+            {
+                events.Add(e);
+                return RedirectToAction(nameof(GetAllEvents));
+            }
+
+            return View(e);
+        }
+
+
+        private void resetTracker()
+        {
+            Event.ID_TRACKER = events.Count + 1;
+        }
+
+
+        private Event findByID(int id)
+        {
+            return events.ElementAt(id - 1);
+        }
+
+
+
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event e = findByID(id);
+            if (e == null)
+            {
+                return NotFound();
+            }
+        
+            return View(e);
+        }
+
+     
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("eventName,eventLocation")] Event e)
+        {
+       
+            if (ModelState.IsValid)
+            {
+              
+                    Event toEdit = findByID(id);
+                    toEdit.eventLocation = e.eventLocation;
+                    toEdit.eventName = e.eventName;
+
+                return RedirectToAction(nameof(GetAllEvents));
+            }
+          
+            return View(e);
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            events.Remove(findByID(id));
+            resetTracker();
+            return View("GetAllEvents" , events);
+        }
+
+          
+    }
+}
---
+++ LAB2_EVENTS/LAB2_EVENTS/Models/Event.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LAB2_EVENTS.Models
+{
+    public class Event
+    {
+        public static int ID_TRACKER = 1;
+
+        public int Id { get; set; }
+
+        [Required]
+        public string eventName { get; set; }
+
+
+        [Required]
+        [MaxLength(30), MinLength(5)]
+        public string eventLocation { get; set; }
+
+        public Event(string eventName, string eventLocation)
+        {
+            this.eventName = eventName;
+            this.eventLocation = eventLocation;
+            this.Id = ID_TRACKER++;
+        }
+
+        public Event()
+        {
+            this.Id = ID_TRACKER++;
+        }
+    }
+}
