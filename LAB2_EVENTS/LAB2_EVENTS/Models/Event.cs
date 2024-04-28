3300737a8347e0cd92086b427abebe9b676a8361
eroolls4 <105580067+eroolls4@users.noreply.github.com>
2024-04-28 15:02:13 +0200

    refactor

--- LAB2_EVENTS/LAB2_EVENTS/Models/Event.cs
+++ LAB2_EVENTS/LAB2_EVENTS/Models/Event.cs
@@ -2,31 +2,20 @@
 
 namespace LAB2_EVENTS.Models
 {
-
-
     public class Event : BaseEvent
     {
         [Required]
         public string eventName { get; set; }
-
-
         [Required]
         [MaxLength(30), MinLength(5)]
         public string eventLocation { get; set; }
-
 
         public Event(string eventName, string eventLocation)
         {
             this.eventName = eventName;
             this.eventLocation = eventLocation;
         }
-
-
+        
         public Event() { }
-
-
-
-
-
     }
 }
