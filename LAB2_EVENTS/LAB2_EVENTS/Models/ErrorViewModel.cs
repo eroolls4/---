4f36f1c0001201a05f0b993bab4b63aa3d8d9ef6
neuralNinj4 <2good4youbye@gmail.com>
2024-04-28 13:54:12 +0200

    Revert "lab2 done"
    This reverts commit f6d875a018959cd95c76831a5e78d0b416c2d7d3.

--- LAB2_EVENTS/LAB2_EVENTS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LAB2_EVENTS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
